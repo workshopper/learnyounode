4b7032fea8d460024a1ce58ca44b6779f603dea0
Rod Vagg <rod@vagg.org>
2013-08-06 11:31:53 +1000

    updated to track use of sync vs async calls

--- problems/my_first_io/setup.js
+++ problems/my_first_io/setup.js
@@ -2,13 +2,43 @@ const boganipsum = require('boganipsum')
     , fs         = require('fs')
     , path       = require('path')
     , os         = require('os')
+    , bold    = require('../../bin/term-util').bold
+    , red     = require('../../bin/term-util').red
+
+function verify (trackFile, callback) {
+  var track   = require(trackFile)
+    , fscalls = track.filter(function (call) {
+        return call.module == 'fs'
+          && call.stack[0].file != 'module.js'
+          && call.stack[0].file != 'fs.js'
+      })
+    , async = fscalls.filter(function (call) {
+        return !/Sync$/.test(call.fn)
+      })
+
+  if (!async.length)
+    return callback() // yay!
+
+  console.log('\nYou got the correct answer but used the following asynchronous calls:')
+  async.forEach(function (call) {
+    console.log('\t' + bold(red('fs.' + call.fn + '()')))
+  })
+  console.log('\nThis problem requires you to only use ' + bold('synchronous') + ' calls.\n')
+  callback('bzzt!')
+}
 
 module.exports = function () {
-  var lines = Math.ceil(Math.random() * 50)
-    , txt   = boganipsum({ paragraphs: lines })
-    , file  = path.join(os.tmpDir(), 'learnyounode_' + process.pid + '.tmp')
+  var lines     = Math.ceil(Math.random() * 50)
+    , txt       = boganipsum({ paragraphs: lines })
+    , file      = path.join(os.tmpDir(), 'learnyounode_' + process.pid + '.tmp')
+    , trackFile = path.join(os.tmpDir(), 'learnyounode_' + process.pid + '.json')
 
   fs.writeFileSync(file, txt, 'utf8')
 
-  return { args: [ file ], stdin: null }
+  return {
+      args        : [ file ]
+    , stdin       : null
+    , modUseTrack : trackFile
+    , verify      : verify.bind(null, trackFile)
+  }
 }
