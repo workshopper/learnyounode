760b8de450122519f6ca1768b88acba765089188
Rod Vagg <rod@vagg.org>
2013-09-19 09:56:14 +1000

    added help text for `learnyounode help`

--- learnyounode.js
+++ learnyounode.js
@@ -4,7 +4,8 @@ const Workshopper = require('workshopper')
     , path        = require('path')
 
 Workshopper({
-    name   : 'learnyounode'
-  , title  : 'LEARN YOU THE NODE.JS FOR MUCH WIN!'
-  , appDir : __dirname
+    name     : 'learnyounode'
+  , title    : 'LEARN YOU THE NODE.JS FOR MUCH WIN!'
+  , appDir   : __dirname
+  , helpFile : path.join(__dirname, 'help.txt')
 }).init()
