eabcd95f856c740dfe0d02d73fc85fa4cbcfb346
lms <ralphtheninja@riseup.net>
2013-09-19 17:45:06 +0200

    forgot to update alternative solution

--- problems/my_first_io/solution.js
+++ problems/my_first_io/solution.js
@@ -7,4 +7,4 @@ console.log(lines)
 // note you can avoid the .toString() by passing 'utf8' as the
 // second argument to readFileSync, then you'll get a String!
 //
-// fs.readFileSync(process.argv[2], 'utf8').split('\n').length
+// fs.readFileSync(process.argv[2], 'utf8').split('\n').length - 1
