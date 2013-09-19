1175a3373834afd130dc5d3fd2b8004f06544565
lms <ralphtheninja@riseup.net>
2013-09-19 17:36:59 +0200

    off by one, empty file is not one line

--- problems/my_first_io/solution.js
+++ problems/my_first_io/solution.js
@@ -1,7 +1,7 @@
 var fs = require('fs')
 
 var contents = fs.readFileSync(process.argv[2])
-var lines = contents.toString().split('\n').length
+var lines = contents.toString().split('\n').length - 1
 console.log(lines)
 
 // note you can avoid the .toString() by passing 'utf8' as the
