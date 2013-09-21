40dd2afb99c1f4a4905945ec928ffa437fde9a38
Rod Vagg <rod@vagg.org>
2013-09-22 09:12:03 +1000

    lots more verification of make it modular module

--- problems/make_it_modular/setup.js
+++ problems/make_it_modular/setup.js
@@ -3,8 +3,8 @@ const fs        = require('fs')
     , os        = require('os')
     , onlyAsync = require('workshopper/verify-calls').verifyOnlyAsync
     , requires  = require('workshopper/fetch-requires')
-    , bold = require('workshopper/term-util').bold
-    , red  = require('workshopper/term-util').red
+    , bold      = require('workshopper/term-util').bold
+    , red       = require('workshopper/term-util').red
 
     , files = [
           'learnyounode.dat'
@@ -23,6 +23,10 @@ const fs        = require('fs')
         , 'wrrrrongdat'
       ]
 
+    , dir       = path.join(os.tmpDir(), 'learnyounode_' + process.pid)
+    , trackFile = path.join(os.tmpDir(), 'learnyounode_' + process.pid + '.json')
+
+
 function verify (trackFile, callback) {
   onlyAsync(trackFile, function (err) {
     if (err)
@@ -31,6 +35,15 @@ function verify (trackFile, callback) {
     requires(trackFile, function (err, main, required) {
       if (err)
         return callback(err)
+
+      var returned  = false
+        , _callback = callback
+        , callbackUsed
+
+      callback = function () {
+        returned = true
+        _callback.apply(this, arguments)
+      }
 
       if (required.length == 1) {
         console.log('\nYou got the correct answer but only used ' + bold('one') + ' file:')
@@ -56,6 +69,15 @@ function verify (trackFile, callback) {
         return callback('bzzt!')
       }
 
+      if (mod.length < 3) {
+        console.log('\nYour additional module file:')
+        console.log('\t' + modfile)
+        console.log('exports a function that takes fewer than ' + bold('three') + ' arguments.')
+        console.log('\nYou must accept a directory, a filter and a ' + bold('callback') + '.\n')
+
+        return callback('bzzt!')
+      }
+
       fs.$readdir = fs.readdir
       var error = new Error('testing')
       fs.readdir = function (dir, callback) {
@@ -72,23 +94,95 @@ function verify (trackFile, callback) {
         return callback('bzzt!')        
       }
 
+      var callbackUsed = false
       try {
         mod('/foo/bar/', 'wheee', function (err) {
-          if (err != error)
+          if (err !== error)
             return noerr()
-          callback() // win!
+
+          callbackUsed = true
         })
       } catch (e) {
         noerr()
       }
+
+      setTimeout(function () {
+        if (returned)
+          return
+
+        if (!callbackUsed) {
+          console.log('\nYour additional module file:')
+          console.log('\t' + modfile)
+          console.log('did not call the callback argument')
+          return callback('bzzt!')
+        }
+
+        fs.readdir = fs.$readdir
+
+        callbackUsed = false
+        try {
+          mod(dir, 'md', function (err, list) {
+            if (err) {
+              console.log('\nYour additional module file:')
+              console.log('\t' + modfile)
+              console.log('returned an error on its callback:')
+              console.log(err)
+              return callback('bzzt!')
+            }
+
+            callbackUsed = true
+
+            var exp = files.filter(function (f) { return /\.md$/.test(f) })
+              , m   = Array.isArray(list) && exp.length === list.length
+              , i   = 0
+              , j
+              , f
+
+            for (; m && i < exp.length; i++) {
+              f = false
+              for (j = 0; m && !f && j < list.length; j++)
+                if (list[j] === exp[i])
+                  f = true
+              if (!f)
+                m = false
+            }
+
+            if (!m) {
+              console.log('\nYour additional module file:')
+              console.log('\t' + modfile)
+              console.log('did not return the correct list of files in an array as the second arugment of the callback')
+              return callback('bzzt!')
+            }
+
+            //WIN!!
+            callback()
+          })
+        } catch (e) {
+          console.log('\nYour additional module file:')
+          console.log('\t' + modfile)
+          console.log('threw an error:')
+          console.log(e)
+
+          return callback('bzzt!')        
+        }
+
+        setTimeout(function () {
+          if (returned)
+            return
+
+          if (!callbackUsed) {
+            console.log('\nYour additional module file:')
+            console.log('\t' + modfile)
+            console.log('did not call the callback argument')
+            return callback('bzzt!')
+          }
+        }, 500)
+      }, 500)
     })
   })
 }
 
 module.exports = function () {
-  var dir       = path.join(os.tmpDir(), 'learnyounode_' + process.pid)
-    , trackFile = path.join(os.tmpDir(), 'learnyounode_' + process.pid + '.json')
-
   fs.mkdirSync(dir)
   files.forEach(function (f) {
     fs.writeFileSync(path.join(dir, f), 'nothing to see here', 'utf8')
