f86d009827561c772646eb8ddc779548e5619a92
Rod Vagg <rod@vagg.org>
2013-12-11 21:22:40 +1100

    added CREDITS menu item

--- learnyounode.js
+++ learnyounode.js
@@ -4,8 +4,9 @@ const Workshopper = require('workshopper')
     , path        = require('path')
 
 Workshopper({
-    name     : 'learnyounode'
-  , title    : 'LEARN YOU THE NODE.JS FOR MUCH WIN!'
-  , appDir   : __dirname
-  , helpFile : path.join(__dirname, 'help.txt')
+    name        : 'learnyounode'
+  , title       : 'LEARN YOU THE NODE.JS FOR MUCH WIN!'
+  , appDir      : __dirname
+  , helpFile    : path.join(__dirname, 'help.txt')
+  , creditsFile : path.join(__dirname, 'credits.txt')
 }).init()
