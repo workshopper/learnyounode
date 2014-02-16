9faeda4f1b9c9e5efbdb2cacdb2cf35595b9fb13
Rod Vagg <rod@vagg.org>
2014-02-16 15:55:45 +1100

    implemented HTTP CLIENT

--- exercises/http_client/solution.js
+++
@@ -1,7 +0,0 @@
-var http = require('http')
-
-http.get(process.argv[2], function (response) {
-  response.setEncoding('utf8')
-  response.on('data', console.log)
-  response.on('error', console.error)
-})
--- exercises/http_client/solution/solution.js
+++ exercises/http_client/solution/solution.js
@@ -1,1 +1,7 @@
-// code stuff here
+var http = require('http')
+
+http.get(process.argv[2], function (response) {
+  response.setEncoding('utf8')
+  response.on('data', console.log)
+  response.on('error', console.error)
+})
