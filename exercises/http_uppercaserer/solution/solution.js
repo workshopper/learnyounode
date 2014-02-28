1c7c40c7c1b5246b3eed43505c43715742371db6
Rod Vagg <rod@vagg.org>
2014-03-01 07:33:58 +1100

    converted http uppercaserer to new workshopper

--- exercises/http_uppercaserer/solution/solution.js
+++ exercises/http_uppercaserer/solution/solution.js
@@ -1,1 +1,13 @@
-// code stuff here
+var http = require('http')
+var map = require('through2-map')
+
+var server = http.createServer(function (req, res) {
+  if (req.method != 'POST')
+    return res.end('send me a POST\n')
+
+  req.pipe(map(function (chunk) {
+    return chunk.toString().toUpperCase()
+  })).pipe(res)
+})
+
+server.listen(Number(process.argv[2]))
