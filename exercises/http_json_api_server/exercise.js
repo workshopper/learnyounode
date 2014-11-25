b3831e8f584c29d11552496aea04703443157b22
Emil Pirfalt <emil.pirfalt@gmail.com>
2014-11-25 17:08:17 +0100

    Bugfix: Error handling function argument. Now takes only the error argument, as intended. Emit the error instead of throwing on `err.message` access. Fixes #93

--- exercises/http_json_api_server/exercise.js
+++ exercises/http_json_api_server/exercise.js
@@ -60,7 +60,7 @@ function query (mode) {
   var exercise = this
 
   function verify (port, stream) {
-    function error (port, err) {
+    function error (err) {
       exercise.emit(
           'fail'
         , 'Error connecting to http://localhost:' + port + ': ' + err.message
