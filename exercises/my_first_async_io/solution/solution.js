6cd3ab843ca5f6adc88bac74064e031d27878671
Martin Heidegger <martin.heidegger@gmail.com>
2014-12-31 13:57:04 +0900

    Added formerly missing standard solution file

---
+++ exercises/my_first_async_io/solution/solution.js
@@ -0,0 +1,8 @@
+var fs = require('fs')
+var file = process.argv[2]
+
+fs.readFile(file, function (err, contents) {
+  // fs.readFile(file, 'utf8', callback) can also be used
+  var lines = contents.toString().split('\n').length - 1
+  console.log(lines)
+})
