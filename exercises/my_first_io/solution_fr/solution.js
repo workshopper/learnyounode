b70a5ef5b63fd7ac2a2cd1c2963238fd3de5a17a
Christophe Porteneuve <tdd@tddsworld.com>
2015-02-15 17:10:14 +0100

    L10n fr for exercise 3: my first I/O

---
+++ exercises/my_first_io/solution_fr/solution.js
@@ -0,0 +1,11 @@
+var fs = require('fs')
+
+var contents = fs.readFileSync(process.argv[2])
+var lines = contents.toString().split('\n').length - 1
+console.log(lines)
+
+// remarquez que vous pouvez éviter d’avoir à appeler `.toString()`
+// en précisant un encodage 'utf8' comme second argument pour
+// `readFileSync`, ce qui vous renverrait une `String` !
+//
+// fs.readFileSync(process.argv[2], 'utf8').split('\n').length - 1
