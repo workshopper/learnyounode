cefeae556ed72ca178b4bea03df4242dface6a6f
Martin Heidegger <martin.heidegger@gmail.com>
2015-11-04 01:41:00 +0900

    no message

--- credits.js
+++ credits.js
@@ -1,23 +1,21 @@
 const fs             = require('fs')
     , path           = require('path')
-    , colorsTmpl     = require('colors-tmpl')
-    , combinedStream = require('combined-stream')
 
 function read (file) {
     return fs.createReadStream(path.join(__dirname, file), {encoding: 'utf8'})
 }
 
 function credits (workshopper) {
-  combinedStream
+  require('combined-stream')
     .create()
-    .append(read ('./i18n/credits/' + workshopper.lang + '.txt'))
+    .append(read ('./i18n/credits/' + workshopper.i18n.lang() + '.txt'))
     .append(read ('./credits.txt'))
     .on("error", function (err) {
       console.log(err)
       throw err
     })
     .on("data", function (data) {
-      console.log(colorsTmpl(data))
+      console.log(require('colors-tmpl')(data))
     })
     .resume()
 }
