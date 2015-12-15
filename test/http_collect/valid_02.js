b9145b4fa7d82a448d85ec5c93b9c1c40d5ba70c
Martin Heidegger <martin.heidegger@gmail.com>
2015-12-15 18:16:17 +0900

    Fixed http_collect test - it is now actually failing

--- test/http_collect/valid_02.js
+++ test/http_collect/valid_02.js
@@ -1,5 +1,6 @@
 require('http').get(process.argv[2], function (response) {
   response.pipe(require('bl')(function (err, data) {
+    data = data.toString()
     console.log(data.length)
     console.log(data)
   }))  
