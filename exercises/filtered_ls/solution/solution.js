7abe53de74bf69b8791789d8aad3e3046db150e1
Sagar <sagartoke510@gmail.com>
2015-12-15 19:53:46 +0530

    Update solution.js
    coding style updated!
    version updated

--- exercises/filtered_ls/solution/solution.js
+++ exercises/filtered_ls/solution/solution.js
@@ -1,14 +1,15 @@
-var fs = require('fs');
-var path = require('path')
+  var fs = require('fs')
+  var path = require('path')
 
-var folder = process.argv[2];
-var ext = '.' + process.argv[3];
+  var folder = process.argv[2]
+  var ext = '.' + process.argv[3]
 
-fs.readdir(folder, function (err, files) {
-    if (err) return console.error(err);
+  fs.readdir(folder, function (err, files) {
+    if (err) return console.error(err)
     files.forEach(function(file) {
         if (path.extname(file) === ext) {
-            console.log(file);
+            console.log(file)
         }
-    });
-});
+    })
+      
+  })
