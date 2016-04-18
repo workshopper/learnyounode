cc33971fd7e5e92250b19b2a8ddbe4ca7b28a96d
Thorn Winter <thornwinter@gmail.com>
2016-04-18 05:21:11 -0400

    Update to non-library solution
    As the instructions ask the student to complete the solution without additional libraries, as a student, I want to see a non-library solution to compare with my own.

--- exercises/juggling_async/solution/solution.js
+++ exercises/juggling_async/solution/solution.js
@@ -1,27 +1,28 @@
-var http = require('http')
-var bl = require('bl')
-var results = []
-var count = 0
+var http = require("http");
+var responses = [];
+var count = 3;
 
-function printResults () {
-  for (var i = 0; i < 3; i++)
-    console.log(results[i])
+function printResponses() {
+    console.log(responses.join('\n'));
 }
 
-function httpGet (index) {
-  http.get(process.argv[2 + index], function (response) {
-    response.pipe(bl(function (err, data) {
-      if (err)
-        return console.error(err)
-
-      results[index] = data.toString()
-      count++
-
-      if (count == 3)
-        printResults()
-    }))
-  })
+function getData(index) {
+    http.get(process.argv[2 + index], function(response) {
+        var data = [];
+        response.setEncoding('utf8');
+        response.on('data', function(string) {
+            data.push(string);
+        });
+        response.on('end', function() {
+           responses[index] = data.join('');
+           count--;
+           if (count === 0) {
+               printResponses();
+           }
+        });
+    });
 }
 
-for (var i = 0; i < 3; i++)
-  httpGet(i)
+for(var i=0; i < 3; i++) {
+    var content = getData(i);
+}
