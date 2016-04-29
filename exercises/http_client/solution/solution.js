2455b91542acaf6ad9befb0c5d112fa2ec4a5523
Martin Heidegger <martin.heidegger@gmail.com>
2016-04-29 20:12:05 +0900

    Readded on("error") handler as voiced in comment https://github.com/workshopper/learnyounode/pull/244#issuecomment-92353238

--- exercises/http_client/solution/solution.js
+++ exercises/http_client/solution/solution.js
@@ -3,4 +3,5 @@ var http = require('http')
 http.get(process.argv[2], function (response) {
   response.setEncoding('utf8')
   response.on('data', console.log)
+  response.on('error', console.error)
 }).on('error', console.error)
