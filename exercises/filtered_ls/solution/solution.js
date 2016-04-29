1df04cef36db7b9ee7c823e6ff77a740ad15bdb1
Martin Heidegger <martin.heidegger@gmail.com>
2016-04-29 19:22:54 +0900

    Fixed indents

--- exercises/filtered_ls/solution/solution.js
+++ exercises/filtered_ls/solution/solution.js
@@ -1,15 +1,14 @@
-  var fs = require('fs')
-  var path = require('path')
+var fs = require('fs')
+var path = require('path')
 
-  var folder = process.argv[2]
-  var ext = '.' + process.argv[3]
+var folder = process.argv[2]
+var ext = '.' + process.argv[3]
 
-  fs.readdir(folder, function (err, files) {
-    if (err) return console.error(err)
-    files.forEach(function(file) {
-        if (path.extname(file) === ext) {
-            console.log(file)
-        }
-    })
-      
+fs.readdir(folder, function (err, files) {
+  if (err) return console.error(err)
+  files.forEach(function(file) {
+      if (path.extname(file) === ext) {
+          console.log(file)
+      }
   })
+})
