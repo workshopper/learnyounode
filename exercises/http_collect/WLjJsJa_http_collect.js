1539e9502af0cbfc4b565a39f1c2d4a0bc7bd3a0
cesargafer <cesargafer@gmail.com>
2016-09-18 15:27:18 -0500

    WLjJsJa add WLjJsJa_http_collect.js

---
+++ exercises/http_collect/WLjJsJa_http_collect.js
@@ -0,0 +1,17 @@
+const http = require('http')
+const bl = require('bl')
+
+http.get(process.argv[2], (res) => {
+ //res.setEncoding('utf8')
+ res.pipe(bl(function(err, data){
+  if (err)
+   return console.error(err)
+ data = data.toString()
+ console.log(data.length)
+ //console.log(data)
+ }))
+})
+
+
+
+
