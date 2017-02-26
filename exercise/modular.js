cb3153ba66b2ada0d9e26873301238c48d9750bc
Soboscott <scott.landry01772@yahoo.com>
2017-02-26 13:53:40 -0500

    Modified from modular.filter to modular_filter

--- exercise/modular.js
+++ exercise/modular.js
@@ -1,14 +1,13 @@
-let filterFn = require('.module.filter.js')
+let filterFn = require('./module_filter.js')
 let dir = process.argv[2]
 let filterStr = process.argv[3]
 
-filterFn(dir, filterStr, function (err, list)
-{
-  if (err) {
-    return console.log('There was an error:', err)
+filterFn(dir, filterStr, function(err, list) {
+    if (err) {
+        return console.error('There was an error:', err)
 
-  }
-  list.forEach(function (file) {
-    console.log(file)
-  })
+    }
+    list.forEach(function(file) {
+        console.log(file)
+    })
 })
