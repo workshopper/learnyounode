e7812c5c37b6db11dd2e70e17aa577f61f6320bb
Soboscott <scott.landry01772@yahoo.com>
2017-02-26 15:01:42 -0500

    Create http_collections.js learned to use bl, but I still don't understand what bl does. I looked in Node.js but could not find a great definition.

---
+++ exercise/http_collection.js
@@ -0,0 +1,14 @@
+let http = require('http')
+let bl = require('bl')
+
+http.get(process.argv[2], function (response){
+  response.pipe(bl(function (err, data) {
+    if (err) {
+      return console.error(err)
+
+    }
+    data = data.toString()
+    console.log(data.length)
+    console.log(data)
+  }))
+})
