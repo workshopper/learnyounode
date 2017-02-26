7484074cc7b8cad79a812a1bd5622dd23df51499
Soboscott <scott.landry01772@yahoo.com>
2017-02-26 15:16:09 -0500

    Create juggle_async.js tried to make this test work, had to look at the soluton page. still not fomfortable with bl.

---
+++ exercise/juggle_async.js
@@ -0,0 +1,31 @@
+var http = require('http')
+var bl = require('bl')
+var results = []
+var count = 0
+
+function printResults () {
+  for (var i = 0; i < 3; i++) {
+    console.log(results[i])
+  }
+}
+
+function httpGet (index) {
+  http.get(process.argv[2 + index], function (response) {
+    response.pipe(bl(function (err, data) {
+      if (err) {
+        return console.error(err)
+      }
+
+      results[index] = data.toString()
+      count++
+
+      if (count === 3) {
+        printResults()
+      }
+    }))
+  })
+}
+
+for (var i = 0; i < 3; i++) {
+  httpGet(i)
+}
