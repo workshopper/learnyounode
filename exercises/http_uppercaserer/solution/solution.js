12b8997450254a9cfce0dcb7114f8b2be3d0236f
Illia Loshchinin <illia.loshchinin@gmail.com>
2017-12-14 00:31:23 +0100

    use standard --fix

--- exercises/http_uppercaserer/solution/solution.js
+++ exercises/http_uppercaserer/solution/solution.js
@@ -1,16 +1,16 @@
 'use strict'
 
-const http = require('http');
-const map = require('through2-map');
+const http = require('http')
+const map = require('through2-map')
 
-const server = http.createServer(function(req, res) {
+const server = http.createServer(function (req, res) {
   if (req.method !== 'POST') {
-    return res.end('send me a POST\n');
+    return res.end('send me a POST\n')
   }
 
-  req.pipe(map(function(chunk) {
-    return chunk.toString().toUpperCase();
-  })).pipe(res);
-});
+  req.pipe(map(function (chunk) {
+    return chunk.toString().toUpperCase()
+  })).pipe(res)
+})
 
-server.listen(Number(process.argv[2]));
+server.listen(Number(process.argv[2]))
