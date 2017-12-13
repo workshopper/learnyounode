7214ff2e87642cd2e4042116b5eecbb43a87578f
Illia Loshchinin <illia.loshchinin@gmail.com>
2017-12-13 23:52:49 +0100

    update exercises to use const and let instead of var #559

--- exercises/http_uppercaserer/exercise.js
+++ exercises/http_uppercaserer/exercise.js
@@ -1,94 +1,98 @@
-var through2 = require('through2')
-var hyperquest = require('hyperquest')
-var exercise = require('workshopper-exercise')()
-var filecheck = require('workshopper-exercise/filecheck')
-var execute = require('workshopper-exercise/execute')
-var comparestdout = require('workshopper-exercise/comparestdout')
-var rndport = require('../../lib/rndport')
-var words = require('boganipsum/clean_words')
-  .sort(function () { return 0.5 - Math.random() })
-  .slice(0, 10)
+'use strict'
+const through2 = require('through2');
+const hyperquest = require('hyperquest');
+let exercise = require('workshopper-exercise')();
+const filecheck = require('workshopper-exercise/filecheck');
+const execute = require('workshopper-exercise/execute');
+const comparestdout = require('workshopper-exercise/comparestdout');
+const rndport = require('../../lib/rndport');
+const words = require('boganipsum/clean_words')
+  .sort(function() {
+    return 0.5 - Math.random();
+  })
+  .slice(0, 10);
 
 // checks that the submission file actually exists
-exercise = filecheck(exercise)
+exercise = filecheck(exercise);
 
 // execute the solution and submission in parallel with spawn()
-exercise = execute(exercise)
+exercise = execute(exercise);
 
 // set up the data file to be passed to the submission
-exercise.addSetup(function (mode, callback) {
-  this.submissionPort = rndport()
-  this.solutionPort = this.submissionPort + 1
+exercise.addSetup(function(mode, callback) {
+  this.submissionPort = rndport();
+  this.solutionPort = this.submissionPort + 1;
 
-  this.submissionArgs = [ this.submissionPort ]
-  this.solutionArgs = [ this.solutionPort ]
+  this.submissionArgs = [this.submissionPort];
+  this.solutionArgs = [this.solutionPort];
 
-  process.nextTick(callback)
-})
+  process.nextTick(callback);
+});
 
 // add a processor for both run and verify calls, added *before*
 // the comparestdout processor so we can mess with the stdouts
-exercise.addProcessor(function (mode, callback) {
-  this.submissionStdout.pipe(process.stdout)
+exercise.addProcessor(function(mode, callback) {
+  this.submissionStdout.pipe(process.stdout);
 
   // replace stdout with our own streams
-  this.submissionStdout = through2()
+  this.submissionStdout = through2();
   if (mode === 'verify') {
-    this.solutionStdout = through2()
+    this.solutionStdout = through2();
   }
 
-  setTimeout(query.bind(this, mode), 500)
+  setTimeout(query.bind(this, mode), 500);
 
-  process.nextTick(function () {
-    callback(null, true)
-  })
-})
+  process.nextTick(function() {
+    callback(null, true);
+  });
+});
 
 // compare stdout of solution and submission
-exercise = comparestdout(exercise)
+exercise = comparestdout(exercise);
 
 // delayed for 500ms to wait for servers to start so we can start
 // playing with them
-function query (mode) {
-  var exercise = this
+function query(mode) {
+  const exercise = this;
 
-  function connect (port, stream) {
-    var input = through2()
-    var count = 0
-    var iv
-    var url = 'http://localhost:' + port
-    var req
+  function connect(port, stream) {
+    const input = through2();
+    let count = 0;
+    let iv;
+    const url = 'http://localhost:' + port;
+    let req;
 
     // TODO: test GET requests for #fail
-    req = input.pipe(hyperquest.post(url)
-      .on('error', function (err) {
+    req = input.pipe(
+      hyperquest.post(url).on('error', function(err) {
         exercise.emit(
-          'fail'
-          , exercise.__('fail.connection', {address: url, message: err.message})
-        )
-      }))
+          'fail',
+          exercise.__('fail.connection', { address: url, message: err.message })
+        );
+      })
+    );
 
-    req.pipe(stream)
-    setTimeout(function () {
-      stream.unpipe(req)
-      stream.end()
-    }, 5000)
+    req.pipe(stream);
+    setTimeout(function() {
+      stream.unpipe(req);
+      stream.end();
+    }, 5000);
 
-    iv = setInterval(function () {
-      input.write(words[count].trim() + '\n')
+    iv = setInterval(function() {
+      input.write(words[count].trim() + '\n');
 
       if (++count === words.length) {
-        clearInterval(iv)
-        input.end()
+        clearInterval(iv);
+        input.end();
       }
-    }, 50)
+    }, 50);
   }
 
-  connect(this.submissionPort, this.submissionStdout)
+  connect(this.submissionPort, this.submissionStdout);
 
   if (mode === 'verify') {
-    connect(this.solutionPort, this.solutionStdout)
+    connect(this.solutionPort, this.solutionStdout);
   }
 }
 
-module.exports = exercise
+module.exports = exercise;
