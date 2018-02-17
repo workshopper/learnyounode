d4c15339e1562afb22ce99d5166c4d54ae235ddf
Andrey.Romanov <Andrey.Romanov@billing.r>
2018-02-17 16:32:18 +0300

    add NAN checking when converting String to Number

--- exercises/baby_steps/solution/solution.js
+++ exercises/baby_steps/solution/solution.js
@@ -1,7 +1,9 @@
 let result = 0
 
 for (let i = 2; i < process.argv.length; i++) {
-  result += Number(process.argv[i])
+  parsedArg = Number(process.argv[i])
+  if (isNaN(parsedArg)) continue
+  result += parsedArg
 }
 
 console.log(result)
