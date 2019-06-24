b384f21ef6874dd44491ecd9c909df2878ac76a0
Kasinath PS <kasinathps@gmail.com>
2019-06-24 13:11:11 +0530

    solution for juggling async without bl

--- exercises/juggling_async/solution/solution.js
+++ exercises/juggling_async/solution/solution.js
@@ -1,21 +1,25 @@
 const http = require("http")
-var required_queue= process.argv.length - 2
+var required_queue = process.argv.length - 2
 var queue = 0
 var urls = process.argv.slice(2)
 var dict = {}
 
 urls.forEach(function (url) {
-	dict[url]=""
-	http.get(url, function (response){
+	dict[url] = ""
+	http.get(url, function (response) {
 		response.setEncoding('utf8')
-		response.on("error", console.error)
-		response.on("data", function(chunk){
-			dict[url]+=chunk
+		response.on("error", function (err) {
+			console.error(err)
 		})
-		response.on("end",function(){
-			queue+=1
-			if (queue == required_queue){
-				urls.forEach(function (key){
+		response.on("data", function (chunk) {
+			dict[url] += chunk
+		})
+		
+		response.on("end", function () {
+			queue++
+			
+			if (queue == required_queue) {
+				urls.forEach(function (key) {
 					console.log(dict[key])
 				})
 			}
