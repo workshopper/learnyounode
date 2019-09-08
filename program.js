117f4703c8ee8ac417d895ce35006bf505343ecf
Ali Kucukavci <ali91kavci@outlook.dk>
2019-09-08 23:21:02 +0200

    Submission

---
+++ program.js
@@ -0,0 +1,12 @@
+//Hello World
+// console.log("HELLO WORLD")
+
+//BABY steps
+
+const sumNumber= (...numbers) =>{
+    return numbers.reduce((a, b) => { return a + b })
+}
+
+console.log(sumNumber(1,2,3))
+
+
