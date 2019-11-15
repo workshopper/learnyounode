9055057f1745682864e1f7ed6922de14b0980d81
manattan <mogimogi0529@eis.hokudai.ac.jp>
2019-11-15 21:23:06 +0900

    fix/practice

--- program5.js
+++ program5.js
@@ -1,10 +1,10 @@
 var fs = require("fs");
 var path = require("path");
 
-var file = process.argv[2];
+var dir = process.argv[2];
 var ext = process.argv[3];
 
-fs.readdir(file, function(err, list) {
+fs.readdir(dir, function(err, list) {
   list.forEach((element) => {
     if (path.extname(element) === "." + ext) {
       console.log(element);
