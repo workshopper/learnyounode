837a2859b2104b376de17e15449e6aa8163e3d7c
XGARBAGE LAB <xgarbagelab@gmail.com>
2020-02-22 15:15:46 +0530

    Update_solution2.js

--- exercises/baby_steps/solution/solution.js
+++ exercises/baby_steps/solution/solution.js
@@ -1,9 +1,8 @@
 'use strict'
-
+let sum = 0;
 function myArgs(args){
-  let sum=0
-  for(let i=0;i<args.length;i++){
-     sum=sum+parseInt(args[i])
+  for(let i = 0;i<args.length;i++){
+     sum = sum + parseInt(args[i])
   }
   console.log(sum)
 }
